e9fb18ddd4456f656b949253d400f98a9f80928c
David Galeano <degnexo@gmail.com>
2016-10-09 16:42:38 -0300

    added some configuration to bens.xml for REST

--- src/main/java/service/testcxf/prime/service/ProbablyPrimeService.java
+++ src/main/java/service/testcxf/prime/service/ProbablyPrimeService.java
@@ -7,14 +7,27 @@ import javax.ws.rs.Produces;
 
 import org.springframework.stereotype.Service;
 
+import kind.Kind;
+import model.LogSystem;
+import model.Profile;
+import model.User;
+
 @Service("probablyPrimeService")
 @Path("/prime")
 public class ProbablyPrimeService {
-	
+
 	@GET
 	@Path("/{number}")
 	@Produces("text/plain")
 	public boolean isPrime(@PathParam("number") int number){
 		return true;
+	}
+	
+	@GET
+	@Path("/profile/{name}")
+	@Produces("application/json")
+	public Profile getProfile(@PathParam("name") String name){
+		Profile profile = new Profile(Kind.ACTION,Kind.ROCK, Kind.FAST_FOOD, 1000);
+		return profile;
 	}
 }
